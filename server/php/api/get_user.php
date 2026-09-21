0da797e741ac2cc097fa3148a2457900da537abe
Pierre Wargnier <orden42@gmail.com>
2026-09-21 13:17:19 +0200

    Get user api

---
+++ server/php/api/get_user.php
@@ -0,0 +1,14 @@
+<?php
+
+use Corrai\Request;
+use Corrai\User;
+
+try {
+    $authorId = Request::get_mandatory_author();
+    $user = User::from_hash($authorId);
+    Request::add_output('user', $user->to_output());
+} catch (\Throwable $th) {
+    Request::handle_throwable($th);
+}
+
+Request::output_all();
