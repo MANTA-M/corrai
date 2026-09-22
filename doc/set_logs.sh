065705c2230f0052226144e35edd754381725c87
Pierre WARGNIER <orden42@gmail.com>
2026-09-22 19:52:17 +0000

    Set logs

---
+++ doc/set_logs.sh
@@ -0,0 +1,5 @@
+ln -s /var/log/nginx/error.log ~/corrai_test/log
+ln -s /var/log/nginx/access.log ~/corrai_test/log
+ln -s /var/log/nginx/mantam-access.log ~/corrai_test/log
+ln -s /var/log/nginx/mantam-error.log ~/corrai_test/log
+ln -s /var/log/corrai/corrai_test_error.log ~/corrai_test/log
